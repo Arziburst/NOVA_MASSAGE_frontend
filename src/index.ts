33aeb246da9016693271c5e8801c55a36c8250be
Belartale <belartale@gmail.com>
2022-06-15 14:37:08 +0300

    after call

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,5 @@ import './main.scss';
 
 import './assets/images/photoMaster.png';
 import './assets/images/imageMakeMassage.png';
+import './assets/images/imageLightBackBody.png';
+import './assets/images/imageBackBody.png';
