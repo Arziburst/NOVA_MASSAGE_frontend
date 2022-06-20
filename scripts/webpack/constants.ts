94fdcd9119d6533536225027f7333063c4efb918
Arziburst <arziburst@gmail.com>
2022-06-20 21:28:26 +0300

    add meta tags

--- scripts/webpack/constants.ts
+++ scripts/webpack/constants.ts
@@ -12,4 +12,4 @@ export const BUILD_DIRECTORY = resolve(PROJECT_ROOT, './build');
 export const nodeModulePath = (nodeModuleName: string) => resolve(PROJECT_ROOT, `./node_modules/${nodeModuleName}`);
 
 // App
-export const APP_NAME = process.env.APP_NAME || 'boilerplate';
+export const APP_NAME = process.env.APP_NAME || 'Nova Massage';
