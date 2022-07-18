0942dff51e4bd9f3b1824fa197c5dcd933ec88ff
Belartale <belartale@gmail.com>
2022-07-18 22:35:41 +0300

    make first screen change language

--- src/index.ts
+++ src/index.ts
@@ -18,3 +18,73 @@ import './components/main';
 import './components/gallery';
 import './components/map';
 import './components/footer';
+
+import { langArrBlockScreen } from './lng';
+
+const allLang = [ '', 'ru', 'en' ];
+
+// const country = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+
+function changeLanguage(selectChangeLang: HTMLSelectElement) {
+    let hash = window.location.hash;
+    hash = hash.substr(1);
+
+    console.log(`hash >>>> ${hash}`);
+    if (!hash) {
+        return;
+    }
+
+    if (!allLang.includes(hash)) {
+        location.href = String(window.location.pathname);
+        location.reload();
+    }
+    selectChangeLang.value = hash;
+
+    for (const keyLangArrBlockScreen in langArrBlockScreen) {
+        if (Object.prototype.hasOwnProperty.call(langArrBlockScreen, keyLangArrBlockScreen)) {
+            const blockScreen = langArrBlockScreen[ keyLangArrBlockScreen ];
+            for (const keyBlockScreen in blockScreen) {
+                if (Object.prototype.hasOwnProperty.call(blockScreen, keyBlockScreen)) {
+                    const element = document.querySelector<any>('#lng_' + keyLangArrBlockScreen + '_' + keyBlockScreen);
+                    const defaultValue = langArrBlockScreen[ keyLangArrBlockScreen ][ keyBlockScreen ].ua;
+                    if (!element) {
+                        return;
+                    }
+
+                    console.log(element.tagName);
+
+                    if (element.tagName === 'IMG') {
+                        element.alt = langArrBlockScreen[ keyLangArrBlockScreen ][ keyBlockScreen ][ hash ];
+                    }
+
+                    if (element) {
+                        element.innerHTML = langArrBlockScreen[ keyLangArrBlockScreen ][ keyBlockScreen ][ hash ];
+                    } else {
+                        element.innerHTML = defaultValue;
+                    }
+                }
+            }
+        }
+    }
+}
+
+function changeURLLanguage(selectChangeLang: HTMLSelectElement) {
+    const lang = selectChangeLang.value;
+    console.log(`lang >>> ${lang}`);
+    //! remove /#
+    location.href = window.location.pathname + '#' + lang;
+    location.reload();
+}
+
+window.addEventListener('load', () => {
+    console.log(`load > window.location.hash >>>> ${window.location.hash}`);
+    const selectChangeLang = document.querySelector<HTMLSelectElement>('#change_lang');
+    if (!selectChangeLang) {
+        return;
+    }
+    changeLanguage(selectChangeLang);
+
+    selectChangeLang.addEventListener('change', () => changeURLLanguage(selectChangeLang));
+});
+
---
+++ src/lng.ts
@@ -0,0 +1,98 @@
+export const langArrBlockScreen: any = {
+    header: {
+        title: {
+            ua: 'Борис Бортніков UA',
+            ru: 'Борис Бортников RU',
+            en: 'Boris Bortnikov EN',
+        },
+        subtitle: {
+            ua: 'Юмейхо-терапевт',
+            ru: 'Юмейхо-терапевт',
+            en: 'Yumeiho-therapist',
+        },
+        nav_link_1: {
+            ua: 'Про мене',
+            ru: 'Про меня',
+            en: 'About me',
+        },
+        nav_link_2: {
+            ua: 'Галерея',
+            ru: 'Галерея',
+            en: 'Gallery',
+        },
+        nav_link_3: {
+            ua: 'Ціни',
+            ru: 'Цены',
+            en: 'Prices',
+        },
+        nav_link_4: {
+            ua: 'Контакти',
+            ru: 'Контакти',
+            en: 'Contacts',
+        },
+    },
+    main: {
+        title: {
+            ua: 'Японська масажна терапія',
+            ru: 'Японская массажная терапия',
+            en: 'Japanese massage therapy',
+        },
+        subtitle: {
+            ua: 'Реабілітаційний масаж у японському виконанні',
+            ru: 'Реабилитационный массаж в японском исполнении',
+            en: 'Japanese Rehabilitation Massage',
+        },
+        photo: {
+            ua: 'Фотографія Бортникова Бориса',
+            ru: 'Фотография Бортникова Бориса',
+            en: 'Photo Bortnikov Boris',
+        },
+        card__title_1: {
+            ua: '3 сеанси',
+            ru: '3 сеанса',
+            en: '3 sessions',
+        },
+        card__text_1: {
+            ua: 'Результат',
+            ru: 'Результат',
+            en: 'Result',
+        },
+        card__title_2: {
+            ua: '5 років',
+            ru: '5 лет',
+            en: '5 years',
+        },
+        card__text_2: {
+            ua: 'Досвіду',
+            ru: 'Опыта',
+            en: 'Experience',
+        },
+        card__title_3: {
+            ua: '1000+',
+            ru: '1000+',
+            en: '1000+',
+        },
+        card__text_3: {
+            ua: 'Задоволених клієнтів',
+            ru: 'Довольных клиентов',
+            en: 'Satisfied customers',
+        },
+        button_1: {
+            ua: 'Записатись',
+            ru: 'Записаться',
+            en: 'Sign up',
+        },
+        button_2: {
+            ua: 'Детальніше про техніку',
+            ru: 'Подробней о технике',
+            en: 'More about technology',
+        },
+    },
+    // test: {
+    //     title: {
+    //         ua: '',
+    //         ru: '',
+    //         en: '',
+    //     },
+    // },
+};
