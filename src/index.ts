86f5d19377d6f7e3cac75eb50b9cd6add0c7a5f4
Belartale <belartale@gmail.com>
2022-07-20 15:20:53 +0300

    change images

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,12 @@ import './main.scss';
 
 // Images
 import './assets/images/photoMaster.png';
-import './assets/images/imageLightBackBody.png';
-import './assets/images/imageBackBody.png';
 import './assets/images/successfulRequestFrom.png';
 
-import './assets/images/primaryImageOfGallery.png';
-import './assets/images/secondaryImageOfGallery.png';
-import './assets/images/tertiaryImageOfGallery.png';
-import './assets/images/quaternaryImageOfGallery.png';
+import './assets/images/primaryImageOfGallery.jpg';
+import './assets/images/secondaryImageOfGallery.jpg';
+import './assets/images/tertiaryImageOfGallery.jpg';
+import './assets/images/quaternaryImageOfGallery.jpg';
 
 // TS Modules
 import './components/main';
